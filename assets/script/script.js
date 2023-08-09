706cf72d8f1a89f8d72f5405c98f1343e473f216
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-08-08 20:03:00 -0700

    Not only single anymore, multiple can be stored.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,20 +1,36 @@
-// Get references to the necessary elements
 var textEntry = document.querySelector("#text-entry");
 var submitButton = document.querySelector("#submitButton");
 var textLocation = document.querySelector("#text-location");
 
-// Add event listener to the submit button
-submitButton.addEventListener("click", function() {
-  // Get the text entered in the textarea
+// Load saved texts from local storage on page load
+function loadTextsFromLocalStorage() {
+  var savedTexts = JSON.parse(localStorage.getItem("enteredTexts")) || [];
+  savedTexts.forEach(function (savedText) {
+    var savedDiv = document.createElement("div");
+    savedDiv.textContent = savedText;
+    textLocation.appendChild(savedDiv);
+  });
+}
+
+function saveTextsToLocalStorage(enteredText) {
+  var savedTexts = JSON.parse(localStorage.getItem("enteredTexts")) || [];
+  savedTexts.push(enteredText);
+  localStorage.setItem("enteredTexts", JSON.stringify(savedTexts));
+}
+
+function handleSubmission() {
   var enteredText = textEntry.value;
-  
-  // Create a new div element to display the entered text
   var newDiv = document.createElement("div");
   newDiv.textContent = enteredText;
-  
-  // Append the new div to the text location div
+
+  saveTextsToLocalStorage(enteredText);
+
   textLocation.appendChild(newDiv);
-  
-  // Clear the textarea for the next input
   textEntry.value = "";
-});
+}
+
+// Load saved texts from local storage on page load
+loadTextsFromLocalStorage();
+
+// Add event listener to the submit button
+submitButton.addEventListener("click", handleSubmission);
