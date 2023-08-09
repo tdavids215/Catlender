51a83913d66349bea6e6cf0320b57d344a041139
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-08-08 23:19:44 -0700

    There is a funtional delete button.(still need to work on it)

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,4 @@
+// Selecting elements from the DOM
 var textEntry = document.querySelector("#text-entry");
 var submitButton = document.querySelector("#submitButton");
 var dateInput = document.querySelector("input[type='date']");
@@ -10,9 +11,24 @@ function displaySavedTexts() {
   var savedTexts = getSavedTexts();
   savedTexts
     .filter((savedText) => savedText.date === selectedDate)
-    .forEach((savedText) => {
+    .forEach((savedText, index) => {
+      
+      // Create a new blockquote element to display saved text
       var savedDiv = document.createElement("blockquote");
       savedDiv.textContent = savedText.text;
+
+      // Create a delete button for each saved text
+      var deleteButton = document.createElement("a");
+      deleteButton.href = "#";
+      deleteButton.role = "button";
+      deleteButton.textContent = "Delete";
+      deleteButton.className = "outline";
+      deleteButton.addEventListener("click", () => handleDelete(index));
+
+      // Append the delete button to the saved text element
+      savedDiv.appendChild(deleteButton);
+
+      // Append the saved text element to the text location
       textLocation.appendChild(savedDiv);
     });
 }
@@ -39,8 +55,22 @@ function handleSubmission() {
   var enteredText = textEntry.value;
   saveTextToLocalStorage(enteredText);
 
+  // Create a new blockquote element for the entered text
   var newDiv = document.createElement("blockquote");
   newDiv.textContent = enteredText;
+
+  // Create a delete button for the newly added text
+  var deleteButton = document.createElement("a");
+  deleteButton.href = "#";
+  deleteButton.role = "button";
+  deleteButton.textContent = "Delete";
+  deleteButton.className = "outline";
+  deleteButton.addEventListener("click", () => handleDelete(savedTexts.length - 1));
+
+  // Append the delete button to the newly added text element
+  newDiv.appendChild(deleteButton);
+
+  // Append the newly added text element to the text location
   textLocation.appendChild(newDiv);
 
   textEntry.value = "";
@@ -49,6 +79,18 @@ function handleSubmission() {
 // Function to handle date selection
 function handleDateSelection() {
   selectedDate = dateInput.value;
+  displaySavedTexts();
+}
+
+// Function to handle deletion of a saved text
+function handleDelete(index) {
+  var savedTexts = getSavedTexts();
+  savedTexts.splice(index, 1); // Remove the selected text
+
+  // Update local storage
+  localStorage.setItem("enteredTexts", JSON.stringify(savedTexts));
+
+  // Re-display saved texts
   displaySavedTexts();
 }
 
