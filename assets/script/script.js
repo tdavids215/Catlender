458b2b6f2d298d08f058ad5248e1775dfd2ddb83
AB <35473718+arsalanbardsiri@users.noreply.github.com>
2023-08-08 21:08:05 -0700

    From local storage, each task is going to be shown only if the related date is chosen.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,36 +1,62 @@
 var textEntry = document.querySelector("#text-entry");
 var submitButton = document.querySelector("#submitButton");
+var dateInput = document.querySelector("input[type='date']");
 var textLocation = document.querySelector("#text-location");
+var selectedDate = null;
 
-// Load saved texts from local storage on page load
-function loadTextsFromLocalStorage() {
-  var savedTexts = JSON.parse(localStorage.getItem("enteredTexts")) || [];
-  savedTexts.forEach(function (savedText) {
-    var savedDiv = document.createElement("div");
-    savedDiv.textContent = savedText;
-    textLocation.appendChild(savedDiv);
-  });
+// Function to display saved texts based on selected date
+function displaySavedTexts() {
+  textLocation.innerHTML = ""; // Clear existing displayed texts
+  var savedTexts = getSavedTexts();
+  savedTexts
+    .filter((savedText) => savedText.date === selectedDate)
+    .forEach((savedText) => {
+      var savedDiv = document.createElement("div");
+      savedDiv.textContent = savedText.text;
+      textLocation.appendChild(savedDiv);
+    });
 }
 
-function saveTextsToLocalStorage(enteredText) {
-  var savedTexts = JSON.parse(localStorage.getItem("enteredTexts")) || [];
-  savedTexts.push(enteredText);
+// Function to get saved texts from local storage
+function getSavedTexts() {
+  return JSON.parse(localStorage.getItem("enteredTexts")) || [];
+}
+
+// Function to save text to local storage
+function saveTextToLocalStorage(enteredText) {
+  var savedTexts = getSavedTexts();
+  savedTexts.push({ date: selectedDate, text: enteredText });
   localStorage.setItem("enteredTexts", JSON.stringify(savedTexts));
 }
 
+// Function to handle text submission
 function handleSubmission() {
+  if (!selectedDate) {
+    alert("Please select a date first.");
+    return;
+  }
+
   var enteredText = textEntry.value;
+  saveTextToLocalStorage(enteredText);
+
   var newDiv = document.createElement("div");
   newDiv.textContent = enteredText;
-
-  saveTextsToLocalStorage(enteredText);
-
   textLocation.appendChild(newDiv);
+
   textEntry.value = "";
 }
 
-// Load saved texts from local storage on page load
-loadTextsFromLocalStorage();
+// Function to handle date selection
+function handleDateSelection() {
+  selectedDate = dateInput.value;
+  displaySavedTexts();
+}
 
-// Add event listener to the submit button
+// Event listener for date selection change
+dateInput.addEventListener("change", handleDateSelection);
+
+// Event listener for submit button click
 submitButton.addEventListener("click", handleSubmission);
+
+// Initial display of saved texts
+displaySavedTexts();
